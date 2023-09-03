ffdf1505886a94ce423b139158869e5ff64a5067
SokolovaV2000 <sokolovava2@mail.ru>
2023-09-03 16:35:00 +0300

    /

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -157,8 +157,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
-EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
-EMAIL_HOST_USER = 'sokolovav2000'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'V45babayagodkaopat'  # пароль от почты
+EMAIL_HOST = 'smtp.yandex.ru' 
+EMAIL_PORT = 465
+EMAIL_HOST_USER = '*'
+EMAIL_HOST_PASSWORD = '*'
 EMAIL_USE_SSL = True
